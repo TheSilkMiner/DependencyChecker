45ba621cd1d0ade230e1ed40afe526a8b9bdded7
Bartek Bok <bartek.bok@gmail.com>
2016-04-16 20:49:10 +0200

    Allow to select working dir via program args

--- src/main/java/openmods/depcheck/DependencyChecker.java
+++ src/main/java/openmods/depcheck/DependencyChecker.java
@@ -3,17 +3,28 @@ package openmods.depcheck;
 import java.io.File;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class DependencyChecker {
 
+    private static final Logger logger = LoggerFactory.getLogger(DependencyChecker.class);
+
     public static void main(String[] args) {
-        final File topDir = new File("data");
-        final SourceParser depWalker = new SourceParser(topDir);
-        final SourceDependencies availableDependencies = depWalker.collectAvailableDependencies();
+        if (args.length == 0)
+            args = new String[] { "data" };
 
-        DependencyCollector collector = new DependencyCollector(availableDependencies);
-        new TargetParser(topDir).accept(collector);
+        for (String dir : args) {
+            final File topDir = new File(dir);
+            logger.info("Processing dir: {}", topDir.getAbsolutePath());
+            final SourceParser depWalker = new SourceParser(topDir);
+            final SourceDependencies availableDependencies = depWalker.collectAvailableDependencies();
 
-        final List<DependencyResolveResult> results = collector.getResults();
-        new ResultPrinter().print(new File(topDir, "output.html"), availableDependencies, results);
+            DependencyCollector collector = new DependencyCollector(availableDependencies);
+            new TargetParser(topDir).accept(collector);
+
+            final List<DependencyResolveResult> results = collector.getResults();
+            new ResultPrinter().print(new File(topDir, "output.html"), availableDependencies, results);
+        }
     }
 }
